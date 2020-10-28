4cd5f066efe40556d262888fc75c494f3fec66e4
Alexis Wilson <alexiswilson@Alexiss-MacBook-Pro.local>
2020-10-28 09:21:26 -0500

    Updating base service route

--- app/src/server.js
+++ app/src/server.js
@@ -6,8 +6,8 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Worldss');
+app.get('/api', (req, res) => {
+  res.send('Hello Worlds');
 });
 
 app.listen(PORT, HOST);
