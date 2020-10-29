0fd271734d0b07fccaf853c1dc67439eb2464de9
= <=>
2020-10-29 16:23:49 -0500

    All kinds of stuff

--- app/src/server.js
+++ app/src/server.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const { Client } = require('pg');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.APP_PORT;
+const HOST = process.env.APP_HOST;
+
+const dbParams = {
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  password: process.env.POSTGRES_PASS,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB
+}
+
+if (process.env.POSTGRES_SSL) {
+  dbParams.ssl = { rejectUnauthorized: false };
+}
+
+const dbClient = new Client(dbParams);
+dbClient.connect();
 
 // App
 const app = express();
 app.get('/api', (req, res) => {
-  res.send('Hello World from the API');
+  dbClient.query('SELECT * FROM speakers', (err, dbres) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    res.send(dbres.rows);
+  });
 });
 
 app.listen(PORT, HOST);
