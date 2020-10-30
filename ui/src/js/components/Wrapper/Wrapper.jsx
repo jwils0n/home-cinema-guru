d2f0449c438a21512551fced7f389084b77c1f8d
= <=>
2020-10-30 16:52:11 -0500

    Fixing it

--- ui/src/js/components/Wrapper/Wrapper.jsx
+++ ui/src/js/components/Wrapper/Wrapper.jsx
@@ -6,18 +6,21 @@ export default class Wrapper extends React.Component {
     constructor(props) {
         super(props);
 
-        axios.get('/api').then(({ data }) => {
-            this.setState({ rows: data });
+        axios.get('/api').then(({ data: rows }) => {
+            this.setState({ rows });
         });
 
         this.state = { rows: [] };
     }
     render() {
+        console.log(this.state);
         return (
             <div className={styles.wrapper}>
                 <h1>Welcome to the Home Cinema Guru!</h1>
                 <p>Here's some raw data:</p>
-                <p>{this.state.rows}</p>
+                <table>
+                    {this.state.rows.map(row => <tr><td>{row.id}</td><td>{row.name}</td><td></td>{row.price}</tr>)}
+                </table>
             </div>
         );
     }
