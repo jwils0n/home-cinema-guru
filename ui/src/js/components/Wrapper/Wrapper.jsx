6ef4e0f1d8343fe993dd3f231343faaa8484dc6c
= <=>
2020-11-04 16:44:30 -0600

    Theming

--- ui/src/js/components/Wrapper/Wrapper.jsx
+++ ui/src/js/components/Wrapper/Wrapper.jsx
@@ -9,6 +9,7 @@ export default class Wrapper extends React.Component {
 
         const testService = new TestService();
         testService.get().then(({ data: rows }) => {
+            console.log(rows);
             this.setState({ rows });
         });
 
@@ -16,29 +17,17 @@ export default class Wrapper extends React.Component {
     }
     render() {
         return (
-            <div className="container">
-                <h1>Welcome to the Home Cinema Guru!</h1>
-                <p>Here's some raw data:</p>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            {/* <td>Image</td> */}
-                            <td>Brand</td>
-                            <td>Name</td>
-                            <td>Price</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.rows.map(row => (
-                            <tr key={row.id}>
-                                {/* <td><img height="100" width="100" src={`${constants.CDN_URL}/speakers/large/${row.slug}.jpg`} /></td> */}
-                                <td>{row.brand.name}</td>
-                                <td>{row.name}</td>
-                                <td>{row.price}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className="container wrapper">
+                <div className="jumbotron">
+                    <h1 className="display-3">The home cinema guru</h1>
+                    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet, tortor ac ultrices ornare, erat nibh faucibus leo.</p>
+                    <hr className="my-4" />
+                    <p>Aenean venenatis a orci at ultrices. Nullam ultrices dignissim nisl. Praesent rutrum, turpis ut ornare gravida, neque nibh auctor nulla, ut semper ligula purus non tortor. Integer venenatis tellus urna, nec feugiat risus pellentesque quis. Mauris dapibus tincidunt dignissim. Aenean pharetra blandit nunc quis vehicula..</p>
+                    <p>
+                        <button className="btn btn-primary btn-lg mr-1">Start from scratch</button>
+                        <button className="btn btn-primary btn-lg">Start using existing components</button>
+                    </p>
+                </div>
             </div>
         );
     }
--- ui/src/js/index.js
+++ ui/src/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Wrapper from './components/Wrapper/Wrapper.jsx';
 import 'bootstrap';
+// import 'bootswatch/dist/lux/bootstrap.min.css';
 import './../scss/base.scss';
 
 ReactDOM.render(<Wrapper />, document.getElementById('react-root'));
